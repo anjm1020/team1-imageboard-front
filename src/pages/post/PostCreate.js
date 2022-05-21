96982f6400dee4d9c5b483aa3691527f881cfb30
jaemin an <jaemin@jaeminui-MacBookPro.local>
2022-05-21 21:11:08 +0900

    [#8] PostCreate 헤더 수정

--- src/pages/post/PostCreate.js
+++ src/pages/post/PostCreate.js
@@ -1,12 +1,16 @@
 import {PostListLayout} from "../../layout";
 import * as Wrapper from "../../component/wrapper";
 import PostCreateForm from "../../component/post/PostCreateForm";
+import SiteName from "../../component/common/SiteName";
+import UserInfo from "../../component/user/UserInfo";
 
 export default () => {
     return (
         <PostListLayout>
-            <Wrapper.Header center>
-                <h2>PostCreate</h2>
+            <Wrapper.Header>
+                <SiteName></SiteName>
+                <h3>PostCreate</h3>
+                <UserInfo user={{name:"testUser"}}></UserInfo>
             </Wrapper.Header>
             <Wrapper.ListBody>
                 <PostCreateForm></PostCreateForm>
