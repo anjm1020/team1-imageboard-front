35e9e394881407aa4a6c44e4694d8b24e216762d
jaemin an <jaemin@jaeminui-MacBookPro.local>
2022-05-21 19:58:58 +0900

    [#0] 프로젝트 생성

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import {Link, Routes, Route} from "react-router-dom";
+import * as Auth from "./pages/auth";
+import * as Post from "./pages/post";
+import NotFound from "./pages/error/NotFound";
+import './App.css';
+
+
+
+function App() {
+    return (
+        <div className="App">
+            <Routes>
+                <Route path="/login" element={<Auth.Login/>}></Route>
+                <Route path="/register" element={<Auth.Register/>}></Route>
+                <Route path="/list" element={<Post.List/>}></Route>
+                <Route path="*" element={<NotFound/>}></Route>
+            </Routes>
+        </div>
+    );
+}
+
+export default App;
