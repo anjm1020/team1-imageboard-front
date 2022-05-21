9751d7759b74039670348c6500247663d36977e7
jaemin an <jaemin@jaeminui-MacBookPro.local>
2022-05-21 21:43:54 +0900

    [#12] url 변경

--- src/App.js
+++ src/App.js
@@ -4,18 +4,16 @@ import * as Post from "./pages/post";
 import NotFound from "./pages/error/NotFound";
 import './App.css';
 
-
-
 function App() {
     return (
         <div className="App">
             <Routes>
                 <Route path="/login" element={<Auth.Login/>}></Route>
                 <Route path="/register" element={<Auth.Register/>}></Route>
-                <Route path="/list" element={<Post.List/>}/>
                 <Route path="/create" element={<Post.Create/>}></Route>
                 <Route path="/update" element={<Post.Update/>}></Route>
                 <Route path="/item" element={<Post.Item/>}></Route>
+                <Route path="/" element={<Post.List/>}/>
                 <Route path="*" element={<NotFound/>}></Route>
             </Routes>
         </div>
