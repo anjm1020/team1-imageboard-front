9995b3895e9005491fe3dfc10fcd758eebdd9c7b
jaemin an <jaeminan@jaeminui-Macmini.local>
2022-05-23 08:33:24 +0900

    [#15] url 라우팅 변경

--- src/App.js
+++ src/App.js
@@ -1,42 +1,18 @@
 import {Routes, Route} from "react-router-dom";
-import {useEffect} from "react";
-import {useDispatch, useSelector} from "react-redux";
-import {loginCheck} from "./module/user";
-import {useNavigate} from "react-router";
 
-import * as Auth from "./pages/auth";
-import * as Post from "./pages/post";
+import Auth from "./route/auth";
+import Post from "./route/post";
 import NotFound from "./pages/error/NotFound";
 import './App.css';
 
 function App() {
 
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-    const username = useSelector(({user}) => user.user);
-
-    useEffect(() => {
-        const token = localStorage.getItem("token");
-        if (!token) navigate("/login");
-
-        if (!username) {
-            dispatch(loginCheck(token));
-            navigate("/login");
-        }
-        // local storage가 지워지는 속도와 memory가 지워지는 속도가 달라서 반짝거림 생김
-
-    }, [navigate, username]);
-
     return (
         <div className="App">
             <Routes>
-                <Route path="/login" element={<Auth.Login/>}></Route>
-                <Route path="/register" element={<Auth.Register/>}></Route>
-                <Route path="/create" element={<Post.Create/>}></Route>
-                <Route path="/update" element={<Post.Update/>}></Route>
-                <Route path="/item" element={<Post.Item/>}></Route>
-                <Route path="/" element={<Post.List/>}/>
-                <Route path="*" element={<NotFound/>}></Route>
+                <Route path="/auth/*" element={<Auth/>}></Route>
+                <Route path="/" element={<Post/>}></Route>
+                <Route path="*" element={<NotFound/>}></Route>}>
             </Routes>
         </div>
     );
---
+++ src/route/auth.js
@@ -0,0 +1,31 @@
+import {useEffect} from "react";
+import {Routes,Route} from "react-router-dom";
+import {useNavigate} from "react-router";
+import {useDispatch,useSelector} from "react-redux";
+
+import * as Auth from "../pages/auth";
+import NotFound from "../pages/error/NotFound";
+import {loginCheck} from "../module/user";
+
+export default () => {
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const username = useSelector(({user}) => user.user);
+
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        if (token&&!username) {
+            dispatch(loginCheck(token));
+        }
+        if (username) navigate("/");
+    }, [navigate, username]);
+
+    return (
+        <Routes>
+            <Route path="/login" element={<Auth.Login/>}></Route>
+            <Route path="/register" element={<Auth.Register/>}></Route>
+            <Route path="*" element={<NotFound/>}></Route>
+        </Routes>
+    );
+}
