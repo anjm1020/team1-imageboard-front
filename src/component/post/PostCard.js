bbaa0c011af55c4d66c1471a9be48f2ffed7dda0
jaemin an <jaemin@jaeminui-MacBookPro.local>
2022-06-13 09:11:40 +0900

    Feat: image card에서 image resizing

--- src/component/post/PostCard.js
+++ src/component/post/PostCard.js
@@ -8,7 +8,7 @@ export default ({post}) => {
 
     return (
         <Card style={{width: '16%'}} className="m-2">
-            <Card.Img variant="top" src={"http://localhost:8080/api/images/" + imgId}/>
+            <Card.Img variant="top" style={{maxHeight:'150px'}} src={"http://localhost:8080/api/images/" + imgId}/>
             <Card.Body>
                 <Card.Title>{title}</Card.Title>
                 <Card.Text>
