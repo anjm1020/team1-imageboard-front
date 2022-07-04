3081e1bd8d91653846080c2f0aae298bfa208cc0
jaemin <jaemin@jaeminui-MacBookPro.local>
2022-07-04 16:09:18 +0900

    Feat : update script

--- bin/setup.sh
+++ bin/setup.sh
@@ -14,10 +14,16 @@ export NVM_DIR="$HOME/.nvm"
 nvm install v16
 
 # git clone
+cd
+git config --global user.email anjm1020@gmail.com
+git config --global user.name jaemin
 git clone https://github.com/BiBimBapXOpenStack/team1-imageboard-front.git
+git fetch --all
+git restore --hard origin/develop
 git pull origin develop
 
 # set nginx configuration
+cd
 sudo rm /etc/nginx/sites-available/default
 sudo rm /etc/nginx/sites-enabled/default
 cd
@@ -34,6 +40,7 @@ cd team1-imageboard-front
 sudo echo REACT_APP_HTTP_URL=$1 >> .env
 
 # build
+cd team1-imageboard-front
 npm install
 npm run build
 
