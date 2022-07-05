3a6b8b7a50bab1eb3bfa18bde2cbc4e1a43f5d46
anGit <55350092+anjm1020@users.noreply.github.com>
2022-07-05 15:11:00 +0900

    Update setup.sh

--- bin/setup.sh
+++ bin/setup.sh
@@ -2,49 +2,76 @@
 # setup.sh
 # @ param = api_server_url
 
-# apt update, install dependency
+
+echo ===========================================
+echo 1. Install Dependency
+echo ===========================================
 sudo apt-get update
 sudo apt-get install nginx git npm -y
 
-# nvm install
+
+echo ===========================================
+echo 2. Node Update
+echo ===========================================
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 nvm install v16
+echo === Node Version ===
+node --version
 
-# git clone
-cd
+
+echo ===========================================
+echo 3. Repository check, Git pull
+echo ===========================================
+cd ~
+[ -d team1-imageboard-front ] || git clone https://github.com/BiBimBapXOpenStack/team1-imageboard-front.git 
 git config --global user.email anjm1020@gmail.com
 git config --global user.name jaemin
-git clone https://github.com/BiBimBapXOpenStack/team1-imageboard-front.git
-cd team1-imageboard-front
+cd ~/team1-imageboard-front
 git fetch --all
 git reset --hard origin/develop
 git pull origin develop
 
-# set nginx configuration
-cd
-sudo rm ~/../../etc/nginx/sites-available/default
-sudo rm ~/../../etc/nginx/sites-enabled/default
-cd
-cd team1-imageboard-front/bin/conf;
+
+echo ===========================================
+echo 4. Nginx Configuration
+echo ===========================================
+[ -f ~/../../etc/nginx/sites-available/default ] && sudo rm ~/../../etc/nginx/sites-available/default
+[ -f ~/../../etc/nginx/sites-enabled/default ] && sudo rm ~/../../etc/nginx/sites-enabled/default
+cd ~/team1-imageboard-front/bin/conf;
 sudo sed -i 's@SERVER_URL@'"$1"'@g' imageboard.conf 
 sudo cp imageboard.conf ~/../../etc/nginx/sites-available/imageboard.conf
-
-# add symbolic link
-cd
+echo === sites-available/imageboard.conf ===
+sudo cat ~/../../etc/nginx/sites-available/imageboard.conf
+cd ~
 sudo ln -s /etc/nginx/sites-available/imageboard.conf /etc/nginx/sites-enabled/imageboard.conf
+echo === sites-enabled/imageboard.conf ===
+sudo cat ~/../../etc/nginx/sites-enabled/imageboard.conf
 
-# make .env file
-cd
-cd team1-imageboard-front
+
+echo ===========================================
+echo 5. React env configuration
+echo ===========================================
+cd ~/team1-imageboard-front/
 sudo echo REACT_APP_HTTP_URL=$1 >> .env
+echo === team1-imageboard-front/.env ===
+sudo cat .env
 
-# build
-cd
-cd team1-imageboard-front
+
+echo ===========================================
+echo 6. Build
+echo ===========================================
+cd ~/team1-imageboard-front
 npm install
 npm run build
+echo === team1-imageboard-front/build/ ===
+cd /build
+ls
 
+
+echo ===========================================
+echo 7. Nginx restart
+echo ===========================================
 sudo systemctl restart nginx
