f11d67b091c89eb5d6f3229c9895014029455c7f
jaemin <anjm1020@gmail.com>
2022-07-05 17:04:22 +0900

    Feat : update script

--- bin/setup.sh
+++ bin/setup.sh
@@ -14,7 +14,7 @@ echo ===========================================
 echo 2. Node Update
 echo ===========================================
 # node version 체크해서 분기처리
-[ -d ~/.nvm ] && {
+[ ! -d ~/.nvm ] && {
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
   export NVM_DIR="$HOME/.nvm"
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
